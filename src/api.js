6d262364516e5e0df64ae94a583b5aa183982e64
David Miller <davidabc66@gmail.com>
2018-05-03 15:58:57 -0400

    added cache headers  to api requests, items should be cached for 30 seconds

--- src/api.js
+++ src/api.js
@@ -1,11 +1,20 @@
 const endPoint = 'https://hacker-news.firebaseio.com/v0';
 
+const config = {
+  cache: 'force-cache',
+  headers: {
+    'cache-control': 'max-age=30',
+  },
+};
+
 export const fetchTopStories = async () =>
-  await fetch(`${endPoint}/topstories.json`).then(response => response.json());
+  await fetch(`${endPoint}/topstories.json`, config).then(response =>
+    response.clone().json()
+  );
 
 export const fetchItem = async (itemId = 1) =>
-  await fetch(`${endPoint}/item/${itemId}.json`).then(response =>
-    response.json()
+  await fetch(`${endPoint}/item/${itemId}.json`, config).then(response =>
+    response.clone().json()
   );
 
 export const fetchItems = async (itemIds = []) =>
