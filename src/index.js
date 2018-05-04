f3e33983b4b2ad77c1855e8214a25b5dd2e3cdb4
David Miller <davidabc66@gmail.com>
2018-05-04 07:54:35 -0400

    added google analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import App from './components/App/App';
 // import registerServiceWorker from './registerServiceWorker';
@@ -14,4 +15,7 @@ const Root = (
 );
 
 render(Root, document.getElementById('root'));
+
+ReactGA.initialize('UA-39752905-6');
+ReactGA.pageview(window.location.pathname + window.location.search);
 // registerServiceWorker();
