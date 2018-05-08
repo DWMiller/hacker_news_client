dbe27b3c9885a4f9401bdca77d31c2279f3e82df
David Miller <davidabc66@gmail.com>
2018-05-08 14:49:00 -0400

    Removed caching 30 second caching rule, was not behaving as expected.

--- src/api.js
+++ src/api.js
@@ -1,19 +1,12 @@
 const endPoint = 'https://hacker-news.firebaseio.com/v0';
 
-const config = {
-  cache: 'force-cache',
-  headers: {
-    'cache-control': 'max-age=30',
-  },
-};
-
 export const fetchStories = async (type = 'top') =>
-  await fetch(`${endPoint}/${type}stories.json`, config).then(response =>
+  await fetch(`${endPoint}/${type}stories.json`).then(response =>
     response.clone().json()
   );
 
 export const fetchItem = async (itemId = 1) =>
-  await fetch(`${endPoint}/item/${itemId}.json`, config).then(response =>
+  await fetch(`${endPoint}/item/${itemId}.json`).then(response =>
     response.clone().json()
   );
 
