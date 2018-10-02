bdcda3f2c957be9e684b41748d0eb2e8ee95f001
David Miller <davidabc66@gmail.com>
2018-10-01 22:44:34 -0400

    updates
    Syncing up bits and pieces with the cra 2.0 baseline

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import ReactGA from 'react-ga';
 
+import * as serviceWorker from './serviceWorker';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 import './styles/global.css';
 import './styles/pagination.css';
 
-const Root = <App />;
-
-render(Root, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 ReactGA.initialize('UA-39752905-6');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-registerServiceWorker();
+serviceWorker.unregister();
