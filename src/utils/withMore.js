eddbc5e1088fc98b7051f90d868642196b22a73a
David Miller <davidabc66@gmail.com>
2018-10-28 13:19:33 -0400

    refactored to use hooks

--- src/utils/withMore.js
+++ src/utils/withMore.js
@@ -1,44 +1,35 @@
-import React, { PureComponent } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const DefaultLoadMoreButton = styled.button``;
 
+const renderItems = (items, Component) => {
+  return items.map(item => {
+    const key = item.id ? item.id : item;
+    return <Component key={key} item={item} />;
+  });
+};
+
 export default (WrappedComponent, LoadMoreButton = DefaultLoadMoreButton) => {
-  return class extends PureComponent {
-    state = {
-      displayedItems: 5,
+  return React.memo((props) => {
+    const [displayedItems, setDisplayedItems] = useState(5);
+
+    const isMoreToLoad = displayedItems < props.items.length;
+
+    const loadMore = () => {
+      setDisplayedItems(displayedItems + 5);
     };
 
-    loadMore = () => {
-      this.setState(state => ({
-        displayedItems: state.displayedItems + 5,
-      }));
-    };
-
-    renderItems(items) {
-      return items.map(item => {
-        const key = item.id ? item.id : item;
-        return <WrappedComponent {...this.props} key={key} item={item} />;
-      });
-    }
-
-    render() {
-      const isMoreToLoad = this.state.displayedItems < this.props.items.length;
-
-      return (
-        <React.Fragment>
-          {this.props.items.length > 0 &&
-            this.renderItems(
-              this.props.items.slice(0, this.state.displayedItems)
-            )}
-          {isMoreToLoad && (
-            <LoadMoreButton onClick={this.loadMore}>
-              Load More - {this.props.items.length - this.state.displayedItems}{' '}
-              Remaining
-            </LoadMoreButton>
-          )}
-        </React.Fragment>
-      );
-    }
-  };
+    return (
+      <React.Fragment>
+        {props.items.length > 0 &&
+          renderItems(props.items.slice(0, displayedItems), WrappedComponent)}
+        {isMoreToLoad && (
+          <LoadMoreButton onClick={loadMore}>
+            Load More - {props.items.length - displayedItems} Remaining
+          </LoadMoreButton>
+        )}
+      </React.Fragment>
+    );
+  });
 };
