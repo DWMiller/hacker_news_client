557ef36152d0a1357acdd51d234365776d28fcca
David Miller <davidabc66@gmail.com>
2019-12-03 16:31:41 -0500

    handle default route

--- src/screens/Root.js
+++ src/screens/Root.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import styled from 'styled-components';
 
 import ScreensStories from 'screens/Stories';
@@ -29,6 +34,7 @@ export const ScreensRoot = () => (
         <Route path="/contact">
           <ContactScreen />
         </Route>
+        <Redirect from="/" to="/stories/top" />
       </Switch>
     </Page>
     <Footer />
